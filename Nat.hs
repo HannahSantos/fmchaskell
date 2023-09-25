7ce4e49ce4dd5d3110bc7ddf4dc00486aa5f1e9f
Hannah Santos <hannah.santos.441@ufrn.edu.br>
2023-09-25 20:27:05 -0300

    Added (+)

--- Nat.hs
+++ Nat.hs
@@ -1,8 +1,15 @@
 module Nat where
 
+import Prelude
+    hiding ((+), (*), (^), quot, min, gcd, lcm, div, max, pred, rem)
+
 data Nat = O | S Nat
     deriving ( Eq , Show )
 
+(+) :: Nat -> (Nat -> Nat)
+n + O = n
+n + (S m) = S (n + m)
+
 double :: Nat -> Nat
 double O = O
-double (S n) = (S (S (double n)))
+double (S n) = S (S (double n))
