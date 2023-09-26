7bac2971eb4984d5a1a1e93afa9d9397a8e521f2
Hannah Santos <hannah.santos.441@ufrn.edu.br>
2023-09-26 14:49:26 -0300

    Silly fixes

--- Nat.hs
+++ Nat.hs
@@ -19,9 +19,8 @@ n ^ O = S O
 n ^ (S m) = n * (n ^ m)
 
 (∸) :: Nat -> Nat -> Nat
-n ∸ O = n
-O ∸ m = O
 (S n) ∸ (S m) = n ∸ m
+n ∸ m = n
 
 double :: Nat -> Nat
 double O = O
@@ -40,11 +39,9 @@ fib (S (S n)) = fib (S n) + fib n
 fib n = n
 
 min :: (Nat, Nat) -> Nat
-min (n, O) = O
-min (O, m) = O
 min (S n, S m) = S (min (n, m))
+min (n, m) = O
 
 max :: (Nat, Nat) -> Nat
 max (n, O) = n
 max (O, m) = m
-max (S n, S m) = S (max (n, m))
